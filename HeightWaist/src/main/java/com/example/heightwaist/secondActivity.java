5263ea41e1672348baece74e3e323691b8415c58
scao7 <scao7@crimson.ua.edu>
2019-05-10 13:40:42 -0500

    ostu

--- HeightWaist/src/main/java/com/example/heightwaist/secondActivity.java
+++ HeightWaist/src/main/java/com/example/heightwaist/secondActivity.java
@@ -3,18 +3,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Picture;
 import android.graphics.PointF;
-
-import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 
 public class secondActivity extends AppCompatActivity {
     PointF pointA = new PointF(530, 40);
@@ -28,8 +21,8 @@ public class secondActivity extends AppCompatActivity {
     Button HeightBtn ;
     Button WaistBtn ;
     Button HipBtn ;
-    Button Otsu;
-    Bitmap image;
+    Button Ostu;
+    PointF[] height;
 
     //create a self define drawLine object
     private drawLine mLineView ;
@@ -41,12 +34,14 @@ public class secondActivity extends AppCompatActivity {
         HeightBtn = findViewById(R.id.Height);
         WaistBtn = findViewById(R.id.Waist);
         HipBtn = findViewById(R.id.Hip);
-        Otsu = findViewById(R.id.Oust);
+        Ostu = findViewById(R.id.Oust);
 
         HeightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawOnCanvas(pointA,pointB,true);
+                Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.sample);
+                height = ostuCalculateHeight(bm);
+                drawOnCanvas(height[0],height[1],true);
             }
         });
         WaistBtn.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +57,7 @@ public class secondActivity extends AppCompatActivity {
             }
         });
 
-        Otsu.setOnClickListener(new View.OnClickListener() {
+        Ostu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent thirdPage = new Intent(getBaseContext(), thirdActivity.class);
@@ -87,7 +82,126 @@ public class secondActivity extends AppCompatActivity {
         mLineView.draw();
     }
 
+    private PointF[] ostuCalculateHeight(Bitmap original){
+        Bitmap BWimg = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
 
+        int width = original.getWidth();
+        int height = original.getHeight();
+        int A, R, G, B, colorPixel;
 
+        double Wcv = 0, th = 0;
+        int[] tPXL = new int[256];
+        int[][] pxl = new int[width][height];
+        double Bw, Bm, Bv, Fw, Fm, Fv;
+        int np, ImgPix = 0, fth = 0;
 
+        // pixel check for histogram //
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                colorPixel = original.getPixel(x, y);
+
+                A = Color.alpha(colorPixel);
+                R = Color.red(colorPixel);
+                G = Color.green(colorPixel);
+                B = Color.blue(colorPixel);
+
+                int gray = (int) ( (0.2126 * R) + (0.7152 * G) + (0.0722 * B) ); // (int) ( (0.299 * R) + (0.587 * G) + (0.114 * B) );
+                pxl[x][y] = gray;
+                tPXL[gray] = tPXL[gray] + 1;
+                ImgPix = ImgPix + 1;
+            }
+        }
+
+        // ----- histo-variance ----- //
+        for (int t = 0; t < 256; t++){
+            Bw = 0; Bm = 0; Bv = 0;
+            Fw = 0; Fm = 0; Fv = 0;
+            np = 0;
+
+            if (t == 0){ // all white/foreground as t0 ----- //
+                Fw = 1;
+
+                for (int d = 0; d < 256; d++) { //mean
+                    Fm = Fm + (d * tPXL[d]);
+                }
+                Fm = Fm / ImgPix;
+
+                for (int e = 0; e < 256; e++) { //variance
+                    Fv = Fv + (Math.pow((e - Fm), 2) * tPXL[e]);
+                }
+                Fv = Fv / ImgPix;
+
+            }
+
+            else { // main thresholding
+                for (int d = 0; d < (t-1); d++){ // BG weight & mean + BG pixel
+                    Bw = Bw + tPXL[d];
+                    Bm = Bm + (d * tPXL[d]);
+                    np = np + tPXL[d];
+                }
+                Bw = Bw / ImgPix;
+                Bm = Bm / np;
+
+                for (int e = 0; e < (t-1); e++) { //BG variance
+                    Bv = Bv + (Math.pow((e - Bm), 2) * tPXL[e]);
+                }
+                Bv = Bv / np;
+
+                for (int j = t; j < 256; j++) { // FG weight & mean + BG pixel
+                    Fw = Fw + tPXL[j];
+                    Fm = Fm + (j * tPXL[j]);
+                    np = ImgPix - np;
+                }
+                Fw = Fw / ImgPix;
+                Fm = Fm / np;
+
+                for (int k = t; k < 256; k++) { //FG variance
+                    Fv = Fv + (Math.pow((k - Fm), 2) * tPXL[k]);
+                }
+                Fv = Fv / np;
+
+            }
+
+            // within class variance
+            Wcv = (Bw * Bv) + (Fw * Fv);
+
+            if (t == 0){
+                th = Wcv;
+            }
+            else if (Wcv < th){
+                th = Wcv;
+                fth = t;
+            }
+        }
+
+        // set binarize pixel
+        float top = Float.POSITIVE_INFINITY;
+        float bottom = Float.NEGATIVE_INFINITY;
+        System.out.println("width is " + width);
+        System.out.println("height is " + height);
+        for (int x = 0; x < width; x++) {
+             for (int y = 0; y < height; y++) {
+
+                int fnpx = pxl[x][y];
+                colorPixel = original.getPixel(x, y);
+
+                A = Color.alpha(colorPixel);
+                if (fnpx < fth) { //R > fth
+
+                    if(top > y ){
+                        top = y;
+                        System.out.println("top is" + top);
+                    }
+                    if(bottom < y){
+                        bottom = y;
+                    }
+                }
+            }
+        }
+        System.out.println(bottom - top);
+        PointF[] points = {new PointF(530,top), new PointF(530,bottom) };
+
+        return points;
+    }
 }
--- HeightWaist/src/main/java/com/example/heightwaist/thirdActivity.java
+++ HeightWaist/src/main/java/com/example/heightwaist/thirdActivity.java
@@ -1,54 +1,19 @@
 package com.example.heightwaist;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+
 
 import static com.example.heightwaist.R.id.OriginalImage;
 
 
 public class thirdActivity extends AppCompatActivity {
-
-    Bitmap bi = null;
-
-    boolean isColored;
-
-    LinearLayout view;
-    LinearLayout view_color;
-
-    boolean flag;
-
-    private int SIZE = 256;
-    // Red, Green, Blue
-    private int NUMBER_OF_COLOURS = 3;
-
-    public final int RED = 0;
-    public final int GREEN = 1;
-    public final int BLUE = 2;
-
-    private int[][] colourBins;
-    private volatile boolean loaded = false;
-    private int maxY;
-
-    private static final int LDPI = 0;
-    private static final int MDPI = 1;
-    private static final int TVDPI = 2;
-    private static final int HDPI = 3;
-    private static final int XHDPI = 4;
-
-    float offset = 1;
-
     ImageView imageView;
     Button LoadImage,ApplyChange,JumpToHisto, Histo;
     Bitmap newBm;
@@ -67,7 +32,7 @@ public class thirdActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(newBm == null )
-                    newBm = convertImage(bm);
+                    newBm = ostuConvert(bm);
                 imageView.setImageBitmap(bm);
             }
         });
@@ -124,6 +89,119 @@ public class thirdActivity extends AppCompatActivity {
         return finalImage;
     }
 
+    public static Bitmap ostuConvert(Bitmap original){
+        Bitmap BWimg = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
 
+        int width = original.getWidth();
+        int height = original.getHeight();
+        int A, R, G, B, colorPixel;
+
+        double Wcv = 0, th = 0;
+        int[] tPXL = new int[256];
+        int[][] pxl = new int[width][height];
+        double Bw, Bm, Bv, Fw, Fm, Fv;
+        int np, ImgPix = 0, fth = 0;
+
+        // pixel check for histogram //
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                colorPixel = original.getPixel(x, y);
+
+                A = Color.alpha(colorPixel);
+                R = Color.red(colorPixel);
+                G = Color.green(colorPixel);
+                B = Color.blue(colorPixel);
+
+                int gray = (int) ( (0.2126 * R) + (0.7152 * G) + (0.0722 * B) ); // (int) ( (0.299 * R) + (0.587 * G) + (0.114 * B) );
+                pxl[x][y] = gray;
+                tPXL[gray] = tPXL[gray] + 1;
+                ImgPix = ImgPix + 1;
+            }
+        }
+
+        // ----- histo-variance ----- //
+        for (int t = 0; t < 256; t++){
+            Bw = 0; Bm = 0; Bv = 0;
+            Fw = 0; Fm = 0; Fv = 0;
+            np = 0;
+
+            if (t == 0){ // all white/foreground as t0 ----- //
+                Fw = 1;
+
+                for (int d = 0; d < 256; d++) { //mean
+                    Fm = Fm + (d * tPXL[d]);
+                }
+                Fm = Fm / ImgPix;
+
+                for (int e = 0; e < 256; e++) { //variance
+                    Fv = Fv + (Math.pow((e - Fm), 2) * tPXL[e]);
+                }
+                Fv = Fv / ImgPix;
+
+            }
+
+            else { // main thresholding
+                for (int d = 0; d < (t-1); d++){ // BG weight & mean + BG pixel
+                    Bw = Bw + tPXL[d];
+                    Bm = Bm + (d * tPXL[d]);
+                    np = np + tPXL[d];
+                }
+                Bw = Bw / ImgPix;
+                Bm = Bm / np;
+
+                for (int e = 0; e < (t-1); e++) { //BG variance
+                    Bv = Bv + (Math.pow((e - Bm), 2) * tPXL[e]);
+                }
+                Bv = Bv / np;
+
+                for (int j = t; j < 256; j++) { // FG weight & mean + BG pixel
+                    Fw = Fw + tPXL[j];
+                    Fm = Fm + (j * tPXL[j]);
+                    np = ImgPix - np;
+                }
+                Fw = Fw / ImgPix;
+                Fm = Fm / np;
+
+                for (int k = t; k < 256; k++) { //FG variance
+                    Fv = Fv + (Math.pow((k - Fm), 2) * tPXL[k]);
+                }
+                Fv = Fv / np;
+
+            }
+
+            // within class variance
+            Wcv = (Bw * Bv) + (Fw * Fv);
+
+            if (t == 0){
+                th = Wcv;
+            }
+            else if (Wcv < th){
+                th = Wcv;
+                fth = t;
+            }
+        }
+
+        // set binarize pixel
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                int fnpx = pxl[x][y];
+                colorPixel = original.getPixel(x, y);
+
+                A = Color.alpha(colorPixel);
+
+                if (fnpx > fth) { //R > fth
+                    fnpx = 255;
+                    BWimg.setPixel(x, y, Color.argb(A, fnpx, fnpx, fnpx));
+                }
+                else {
+                    fnpx = 0;
+                    BWimg.setPixel(x, y, Color.argb(A, fnpx, fnpx, fnpx));
+                }
+            }
+        }
+        return BWimg;
+    }
 
 }
